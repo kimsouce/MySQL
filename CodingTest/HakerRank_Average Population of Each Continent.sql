a77789e3befeb3924016d611ac03a2b969dd38b6
Kimsouce <60845730+kimsouce@users.noreply.github.com>
2020-12-24 05:02:03 +0900

    Create HakerRank_Average Population of Each Continent.sql

---
+++ CodingTest/HakerRank_Average Population of Each Continent.sql
@@ -0,0 +1,16 @@
+/*
+문제:
+Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) 
+rounded down to the nearest integer.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+
+SELECT COUNTRY.Continent, FLOOR(AVG(CITY.Population))
+FROM city
+    INNER JOIN country ON CITY.CountryCode = COUNTRY.Code
+GROUP BY country.continent 
+
+/*
+1. country의 continent 별로 city population이 나와야 하므로, 집계함수를 쓰기 위해 GROUP BY를 해주는 것이다. 
+*/
